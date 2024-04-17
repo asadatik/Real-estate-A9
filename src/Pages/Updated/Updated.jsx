fc229837857623414805e386fedbabf5c4db29e2
Asad Atik <asadatik1995@gmail.com>
2024-04-17 17:37:43 +0600

    Added footer section

--- src/Pages/Updated/Updated.jsx
+++ src/Pages/Updated/Updated.jsx
@@ -1,13 +1,13 @@
 import { useContext, useEffect, useState } from 'react';
 
-// import toast from 'react-hot-toast';
 
-// import { Helmet } from 'react-helmet-async';
+
+import { Helmet } from 'react-helmet-async';
 import { AuthContext } from '../../Provider/AuthProvider';
                 
 const Updated = () => {           
     const { user, updatedUserProfile} = useContext(AuthContext); 
-    console.log(user)
+    console.log(user) 
     const [Name, setName] = useState('');
     const [photo, setPhoto] = useState('');
 
@@ -16,12 +16,14 @@ const Updated = () => {
            setPhoto(user.photoURL|| '');
           }, [user]);
  const ChanceProfile = () => {
-    updatedUserProfile(  Name, photo)   
-                  .then(() => {
-                    // toast.success("Profile updated successfully!");
+        updatedUserProfile(Name, photo)   
+                   .then((  ) => {
+                         
+                         
                   })
                   .catch((error) => {
-                    // toast.error("Error updating profile: " + error.message);
+                  
+                    console.error(error.message)
                   });
               };    
 
@@ -30,11 +32,11 @@ const Updated = () => {
 
     return (
         <div className='flex flex-col justify-center items-center md:h-[75vh]'>
-           
+           <Helmet>   <title> Update Profile  </title>   </Helmet>
       <div className='md:flex w-full  justify-center gap-5 mt-2 md:gap-10'>
-                <div className='space-y-3 flex flex-col items-center border-2 border-green-500 p-2 md:p-10'>
-                 <h2 className='text-3xl font-extrabold border-b-4 border-cyan-400 tracking-[.25em] uppercase'>Profile</h2>
-              <img data-aos="zoom-in" data-aos-duration="1000" className='ring-4 ring-pink-500 rounded-full w-48 md:w-52' src={user?.photoURL} alt="" />
+                <div className='space-y-3 flex flex-col items-center border-2 border-lime-950 p-2 md:p-10'>
+                 <h2 className='text-3xl font-extrabold border-b-4 tracking-[.25em] uppercase'>Profile</h2>
+              <img data-aos="zoom-in" data-aos-duration="1000" className='rounded-full w-48 md:w-52' src={user?.photoURL} alt="" />
              <h4 data-aos="zoom-out-right" data-aos-duration="1200" className="text-xl md:text-2xl font-bold">Name : {user?.displayName}</h4>
                   <p data-aos="zoom-out-right" data-aos-duration="1400" className='text-sm md:text-xl font-semibold'>Email : {user?.email || "Email is not found"}</p>
                   
@@ -49,9 +51,11 @@ const Updated = () => {
                      <input className="input input-bordered input-error w-full max-w-xs " type="text" value={Name} onChange={(e) => setName(e.target.value)} />
                      <input className="input input-bordered input-error w-full max-w-xs " type="text" value={photo} onChange={(e) => setPhoto(e.target.value)} />
                    </div>
-                   <button className='btn w-fit mx-auto btn-primary animate__animated' onClick={ChanceProfile}>Update Profile</button>     
+                   <button className='btn w-fit mx-auto btn-secondary' onClick={ChanceProfile}>Update Profile</button>     
+                 
                  </div>
                </div>
+             
              </div>
         )}
     export default Updated;
