8a08d63165d3bc6521921e0c11d68026fb472db5
Asad Atik <asadatik1995@gmail.com>
2024-04-18 19:26:40 +0600

    Edited Updated Profile Section

--- src/FireBase/FireBase.config.js
+++ src/FireBase/FireBase.config.js
@@ -5,12 +5,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  // apiKey: "AIzaSyDZ_YDJ_A3H9aAY2Kzl57RUcgPpoUv0-yM",
-  // authDomain: "real-estate-bussiness-88e8c.firebaseapp.com",
-  // projectId: "real-estate-bussiness-88e8c",
-  // storageBucket: "real-estate-bussiness-88e8c.appspot.com",
-  // messagingSenderId: "623927081999",
-  // appId: "1:623927081999:web:3cdc50debe0ca6a2e9f77a" 
+   
   apiKey:import.meta.env.VITE_APIKEY ,
   authDomain:import.meta.env. VITE_AUTHDOMAIN ,
 
